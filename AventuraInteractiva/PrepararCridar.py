b93dc61ee0a56439ff543750cb1f160921f51a6a
Nat121312 <bernatpuigcasals@gmail.com>
2026-02-09 19:30:45 +0100

    Carrega de CSV creada, converteix els valors al tipus indicat i els coloca en un diccionari dins d'una llista

--- AventuraInteractiva/PrepararCridar.py
+++ AventuraInteractiva/PrepararCridar.py
@@ -25,26 +25,38 @@ def CallCSV(cami):
 
         with open(ruta, "r", encoding="utf-8") as Entities:
             data = Entities.read().splitlines(False)
-            caps = data[0].strip().replace(" ", "").split(";")
+            caps = data[0].strip().split(";")
 
             Data = []
             DictData = {}
             for i in data[1:]:
-                linia = i.strip().replace(" ", "").split(";")
+                linia = i.strip().split(";")
                 for j in range(len(linia) -1):
-                    
+                    linia[j] = linia[j].strip()
+                    if caps[j].endswith("?"):
+                        linia[j] = bool(linia[j])
+                    elif linia[j].isnumeric():
+                        linia[j] = int(linia[j])
+                    elif len(linia[j].split(": ")) > 1:
+                        items = linia[j].split(", ").copy()
+                        linia[j] = dict()
+                        for v in items:
+                            kv = v.split(": ")
+                            linia[j][kv[0]] = int(kv[1]) if not kv[1].endswith("%") else kv[1]
+                    elif len(linia[j].split(", ")) > 1:
+                        linia[j] = linia[j].split(", ")
                     DictData[caps[j]] = linia[j]
                 Data.append(DictData.copy())
             print(Data)
 
 
-    except ValueError:
-        print("Ha ocurregut un error carregant els moviments...")
+    except FileNotFoundError:
+        print("Ha ocurregut un error carregant el fitxer...")
 
 
 def main():
     print("!! - Joc de Preguntes - !!")
-    CallCSV("Data/Objects.csv")
+    CallCSV("Data/EntityTypes.csv")
 
 if __name__ == "__main__":
     main()
