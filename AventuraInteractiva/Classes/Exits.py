a47d7ba6255b538a65aa2b06de1c29dca6d93d7f
Nat121312 <bernatpuigcasals@gmail.com>
2026-03-16 16:28:29 +0100

    Començades a crear els metodes dels exits..., de moment creat el de reclamar les recompenses (només per a estadistiques de moment), i el de desbloqueig, no el de comprovació, per al de comprovació caldra cridar-lo desde ControladorExits

--- AventuraInteractiva/Classes/Exits.py
+++ AventuraInteractiva/Classes/Exits.py
@@ -20,5 +20,24 @@ class Exits():
         self.Obtained = False
         self.Rewards = rewards
         self.UnlockRequirements = unlock
+    
+
+    def UnlockExit(self, jugador):
+        self.Obtained = True
+        jugador.AcquiredAchievements.append(self.id)
+        self.ClaimRewards(jugador)
+
+    def ComprovarExit(self, jugador):
+        print()
+
+    def ClaimRewards(self, jugador):
+        for key, value in self.rewards.items():
+            if self.unlock["Type"] == "Stat":
+                tipus = "Flat"
+                if isinstance(value, str):
+                    if value.endswith("%"):
+                        tipus = "%"
+                        value.replace("%", "")
+                jugador.StatIncrement[key][tipus]+=value
 
         
